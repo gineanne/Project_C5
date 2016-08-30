f744b2c5ccf9482e427ce2156c0224f80cb4b664
Horlador, Elgine <ELGINE_HORLADOR@homedepot.com>
2016-08-30 16:18:45 +0800

    Local Commit 2

--- src/First_Class.java
+++ src/First_Class.java
@@ -3,16 +3,5 @@ public class First_Class {
 	
 	public static void main (String [] args) {
 		System.out.println("Sample!");
-		System.out.println("Sample!");
-		System.out.println("Sample!");
-		System.out.println("Sample!");
-		System.out.println("Sample!");
-		System.out.println("Sample!");
-	}
-	
-	public String test () {
-		String a = "test 1";
-		
-		return a;
 	}
 }
