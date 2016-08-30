6741c0cf962075ca45eaa976a41eb3a5d5471a21
Horlador, Elgine <ELGINE_HORLADOR@homedepot.com>
2016-08-30 16:05:04 +0800

    Local commit 1

--- src/First_Class.java
+++ src/First_Class.java
@@ -4,5 +4,15 @@ public class First_Class {
 	public static void main (String [] args) {
 		System.out.println("Sample!");
 		System.out.println("Sample!");
+		System.out.println("Sample!");
+		System.out.println("Sample!");
+		System.out.println("Sample!");
+		System.out.println("Sample!");
+	}
+	
+	public String test () {
+		String a = "test 1";
+		
+		return a;
 	}
 }
